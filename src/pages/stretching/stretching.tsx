99600e05a0ce337c6b9203252a9c1b6897877952
Erik Töger <erik.toger@gmail.com>
2022-10-17 17:49:32 +0200

    Add Stretching

--- src/pages/stretching/stretching.tsx
+++ src/pages/stretching/stretching.tsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import { Stretches } from "./constants";
+type StretchingKeys = keyof typeof Stretches;
+type StretchingValue = typeof Stretches[StretchingKeys];
+
 const Stretching = () => {
-  return <div>Streching page</div>;
+  const [stretchPosition, setStretchPosition] = useState<
+    [string, StretchingValue]
+  >(["sitting", Stretches.sitting]);
+  return (
+    <div>
+      <h1>Streching page</h1>
+      <div>
+        {Object.entries(Stretches).map((entry) => {
+          return (
+            <button onClick={() => setStretchPosition(entry)}>
+              {entry[0]}
+            </button>
+          );
+        })}
+      </div>
+      <h3>{stretchPosition[0]}</h3>
+      <ul>
+        {stretchPosition[1].map((position) => (
+          <li>{position}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Stretching;
