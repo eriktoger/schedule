411fb7f64ee5faa2f1a315078ee692c8ff266ba9
Erik Töger <erik.toger@gmail.com>
2024-07-11 18:49:57 +0200

    Update traning page

--- src/pages/training/constants.ts
+++ src/pages/training/constants.ts
@@ -5,24 +5,34 @@ export const TrainingDays = [
     finisher: "Sledge",
   },
   {
+    title: "Tue",
+    exercises: ["Squats", "Swings"],
+    finisher: "Ring hang",
+  },
+  {
     title: "Wed",
-    exercises: ["Lunges", "Dips/Push press", "Pull ups"],
+    exercises: ["Dips", "Pull ups"],
     finisher: "Jumps",
   },
   {
-    title: "Fri",
+    title: "Thu",
     exercises: ["Clean and Press", "Curls"],
+    finisher: "Core",
+  },
+  {
+    title: "Fri",
+    exercises:["One leg squat", "Push press"],
     finisher: "Sandbag",
   },
   {
     title: "Sat",
-    exercises: ["Hillsprint"],
-    finisher: "",
+    exercises: ["Push ups", "Rows"],
+    finisher: "Over head carry",
   },
   {
     title: "Sun",
-    exercises: ["One leg squat", "Push ups", "Row"],
-    finisher: "Core",
+    exercises:["RDL","Over head squat"]  ,
+    finisher: "No finisher",
   },
 ] as const;
 
